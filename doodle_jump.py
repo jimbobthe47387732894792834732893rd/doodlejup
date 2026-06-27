6a78d5ec365a2f688af90999f8ee7ce774247851
Arshawn Derakhshan <arshawn.nd@gmail.com>
2026-06-27 09:53:47 -0700

    added player

--- doodle_jump.py
+++ doodle_jump.py
@@ -1,8 +1,12 @@
 import pygame
 pygame.init()
 
+# configuration variables
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = SCREEN_WIDTH * 1.5
+
 # pygame setup
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
@@ -14,9 +18,10 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("#003359")
+    screen.fill("#FFF1DC")
 
     # RENDER YOUR GAME HERE
+    pygame.draw.rect(screen,"green",(250,250,SCREEN_WIDTH / 7,SCREEN_WIDTH / 6))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
